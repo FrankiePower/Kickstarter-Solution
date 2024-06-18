1e3a4d51a57d8e23b132585092cb5dc5eb31ce90
FrankiePower <franklin.power21@gmail.com>
2024-06-18 15:49:04 +0100

    Mocha Tests

--- test/campaign-test.js
+++ test/campaign-test.js
@@ -5,8 +5,6 @@ const web3 = new Web3(ganache.provider());
 
 const compiledFactory = require("../ethereum/build/CampaignFactory.json");
 const compiledCampaign = require("../ethereum/build/Campaign.json");
-const { validateHeaderName } = require("http");
-
 let accounts;
 let factory;
 let campaignAddress;
