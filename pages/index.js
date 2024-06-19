9be1a7318cf6f1265888ce284d8579ccf6207dde
FrankiePower <franklin.power21@gmail.com>
2024-06-19 12:14:43 +0100

    updates to package.json

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
+import { Card, Button } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
 import factory from "../ethereum/factory";
+
+/* import layout from "../components/layout"; */
 
 class CampaignIndex extends Component {
   static async getInitialProps() {
@@ -8,8 +12,33 @@ class CampaignIndex extends Component {
     return { campaigns };
   }
 
+  renderCampaigns() {
+    const items = this.props.campaigns.map((address) => {
+      return {
+        header: address,
+        description: <a>View Campaign</a>,
+        fluid: true,
+      };
+    });
+
+    return <Card.Group items={items} />;
+  }
+
+  renderButton() {}
+
   render() {
-    return <div>{this.props.campaigns[0]}</div>;
+    return (
+      <div>
+        <h2>Open Campaigns</h2>
+        {this.renderCampaigns()}
+        <Button
+          content="Create Campaign"
+          icon="add circle"
+          labelPosition="right"
+          primary
+        />
+      </div>
+    );
   }
 }
 
