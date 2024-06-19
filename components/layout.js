f743ea84f7649407e1148ae26c26a6be995b279d
FrankiePower <franklin.power21@gmail.com>
2024-06-19 14:49:15 +0100

    Update to the Layout

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,15 @@
 import React from "react";
+import Header from "./header";
+import { Container } from "semantic-ui-react";
 
 const layout = (props) => {
   return (
-    <div>
-      <h1>I am a header</h1>
-      {props.children}
-      <h1>I am a footer</h1>
-    </div>
+    <Container>
+      <div>
+        <Header />
+        {props.children}
+      </div>
+    </Container>
   );
 };
 export default layout;
